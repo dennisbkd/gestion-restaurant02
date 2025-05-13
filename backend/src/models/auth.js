e71296af3200b57d6d7a509eaa84682964e9a54d
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-05-13 00:03:09 -0400

    Implementación de autenticación y autorización con JWT y Cookies, incluyendo nuevas rutas y controladores para el administrador y la autenticación de usuarios.

--- backend/src/models/auth.js
+++ backend/src/models/auth.js
@@ -4,6 +4,10 @@ import bcrypt from 'bcrypt'
 import { definicionUsuario } from '../services/user.js'
 
 export class ModeloAuth {
+  constructor (token) {
+    this.token = token
+  }
+
   static Usuario = sequelize.define('Usuario', definicionUsuario, {
     timestamps: false,
     freezeTableName: true
@@ -19,15 +23,21 @@ export class ModeloAuth {
       if (!buscarUsuario) return { error: 'Usuario no encontrado' }
       const verificarPassword = bcrypt.compare(password, buscarUsuario.password)
       if (!verificarPassword) return { error: 'Password incorrecto' }
+      const nuevoToken = this.token.crearToken({
+        id: buscarUsuario.id,
+        nombreUsuario: buscarUsuario.nombreUsuario,
+        rol: buscarUsuario.idRol
+      })
       return {
         user: {
           nombreUsuario: buscarUsuario.nombreUsuario,
-          password: buscarUsuario.correo,
+          correo: buscarUsuario.correo,
           rol: buscarUsuario.idRol
-        }
+        },
+        nuevoToken
       }
     } catch (error) {
-      throw new Error(error)
+      throw new Error('Error al loguearse')
     }
   }
 
@@ -38,36 +48,60 @@ export class ModeloAuth {
         where: { nombreUsuario }
       })
 
-      if (buscarEmpleado) return { error: 'El empleado ya está registrado' }
+      if (!buscarEmpleado) return { error: 'El empleado ya está registrado' }
       const buscarCorreo = await this.Usuario.findOne({
         where: { correo }
       })
 
-      if (buscarCorreo) return { error: 'El correo ya existe' }
+      if (!buscarCorreo) return { error: 'El correo ya existe' }
 
       const hashPassword = bcrypt.hashSync(password, 10)
-      await sequelize.query(
-        'EXEC registrarEmpleado @nombreUsuario = :nombreUsuario, @nombre = :nombre, @password = :password, @correo = :correo, @telefono = :telefono, @idRol = :idRol, @ci = :ci',
-        {
-          replacements: {
-            nombreUsuario,
-            nombre,
-            password: hashPassword,
-            correo,
-            telefono,
-            idRol,
-            ci
+      try {
+        await sequelize.query(
+          'EXEC registrarEmpleado @nombreUsuario = :nombreUsuario, @nombre = :nombre, @password = :password, @correo = :correo, @telefono = :telefono, @idRol = :idRol, @ci = :ci',
+          {
+            replacements: {
+              nombreUsuario,
+              nombre,
+              password: hashPassword,
+              correo,
+              telefono,
+              idRol,
+              ci
+            }
           }
-        }
-      )
+        )
+      } catch (e) {
+        throw new Error('Error al llamar el procedimiento almacenado')
+      }
+      const nuevoToken = this.token.crearToken({
+        id: buscarEmpleado.id,
+        nombreUsuario: buscarEmpleado.nombreUsuario,
+        rol: buscarEmpleado.idRol
+      })
       return {
-        user: {
-          idRol,
-          ci
-        }
+        empleado: {
+          nombreUsuario: buscarEmpleado.nombreUsuario,
+          correo: buscarEmpleado.correo,
+          rol: buscarEmpleado.idRol
+        },
+        nuevoToken
       }
     } catch (error) {
-      throw new Error(error)
+      throw new Error('Error al crearse un nuevo empleado')
+    }
+  }
+
+  static async perfil ({ input }) {
+    const id = input.id
+    const user = await this.Usuario.findByPk(id)
+    if (!user) return { error: 'Error: Usuario  no existente' }
+    return {
+      user: {
+        email: user.correo,
+        userName: user.nombreUsuario,
+        rol: user.idRol
+      }
     }
   }
 }
--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import { ControladorAuth } from '../controllers/auth.js'
+import { autenticacion, clearToken } from '../../middleware/authPermiso.js'
 
 export const crearAuthRutas = ({ modeloAuth }) => {
   const Authruta = Router()
+  const middleware = autenticacion(modeloAuth.token)
   const controladorAuth = new ControladorAuth({ modeloAuth })
   Authruta.post('/login', controladorAuth.login)
   Authruta.post('/register', controladorAuth.registrarEmpleado)
-  Authruta.post('/logout')
-  Authruta.post('/verificar')
+  Authruta.get('/logout', clearToken)
+  Authruta.get('/verificar', middleware, controladorAuth.perfil)
   return Authruta
 }
