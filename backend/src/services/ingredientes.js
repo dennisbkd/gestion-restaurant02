6d22e532195d9bfde0212d90895a49c993b9dfb0
dennisbkd <rasguidorondalesdennis@gmail.com>
2025-05-16 20:46:30 -0400

    ordenando los archivos

---
+++ backend/src/services/ingredientes.js
@@ -0,0 +1,25 @@
+import { DataTypes } from 'sequelize'
+export const definicionIngrediente = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nombre: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true
+  },
+  idUnidadMedida: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  idStock: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  idEstado: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+}
