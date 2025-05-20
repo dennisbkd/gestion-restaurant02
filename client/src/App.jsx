c6fc0cf54b6f84be3c79811008fa610a953f7ac7
SebasL <sebas91ts5@gmail.com>
2025-05-20 16:48:44 -0400

    implementacion de sidebar con rutas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,52 @@
-import { BrowserRouter, Routes, Route } from "react-router"
-import RegisterPage from "./pages/RegisterPage"
-import LoginPage from "./pages/LoginPage"
-import { AuthProvide } from "./context/AuthContext"
-import { DashboardPage } from "./pages/DashboardPage"
-import { ProfilePage } from "./pages/ProfilePage"
-import { HomePage } from "./pages/HomePage"
-import { ProtectedRoute } from "./ProtectedRoute"
-import { Task } from "./pages/task"
-import Menu from "./components/usuario/Menu"
-import { lazy, Suspense } from "react"
-import { CartLayout } from "./Layouts/CartLayout"
-import { CargaDeEspera } from "./components/loading/CargaDeEspera"
+import { BrowserRouter, Routes, Route } from 'react-router'
+import RegisterPage from './pages/RegisterPage'
+import LoginPage from './pages/LoginPage'
+import { AuthProvide } from './context/AuthContext'
+import DashboardRoutes from './routes/AdminRoutes'
+import { ProfilePage } from './pages/ProfilePage'
+import { HomePage } from './pages/HomePage'
+import { ProtectedRoute } from './ProtectedRoute'
+import { Task } from './pages/task'
+import Menu from './components/usuario/Menu'
+import { lazy, Suspense } from 'react'
+import { CartLayout } from './Layouts/CartLayout'
+import { CargaDeEspera } from './components/loading/CargaDeEspera'
 
 const CheckoutPage = lazy(() => import('./pages/procesoPago/CheckoutPage'))
-
 
 export default function App() {
   // todas las rutas hijas tendran el contexto
   return (
     <AuthProvide>
       <BrowserRouter>
-
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/task" element={<Task />} />
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/profile' element={<ProfilePage />} />
+          <Route path='/task' element={<Task />} />
 
-
-          <Route element={<ProtectedRoute />}>
-            <Route path="/dashboard" element={<DashboardPage />} />
-          </Route>
+          <Route element={<ProtectedRoute />}>{DashboardRoutes()}</Route>
           <Route element={<CartLayout />}>
-            <Route path="/productos" element={<Menu />} />
-            <Route path="/checkout" element={<Suspense fallback={<CargaDeEspera
-              text="Procesando tu pedido..."
-              text2="Redirigiendo al método de pago" />}>
-              <CheckoutPage />
-            </Suspense>} />
+            <Route path='/productos' element={<Menu />} />
+            <Route
+              path='/checkout'
+              element={
+                <Suspense
+                  fallback={
+                    <CargaDeEspera
+                      text='Procesando tu pedido...'
+                      text2='Redirigiendo al método de pago'
+                    />
+                  }
+                >
+                  <CheckoutPage />
+                </Suspense>
+              }
+            />
           </Route>
         </Routes>
       </BrowserRouter>
-    </AuthProvide >
-  );
+    </AuthProvide>
+  )
 }
