3c659e8cea964ca8f1440b5a395ef2408824acb1
SebasL <sebas91ts5@gmail.com>
2025-05-22 14:43:45 -0400

    Mover carpetas a admin y agregar apartado de inventario

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router'
-import SideBar from '../components/SideBar'
+import SideBar from '../components/AdminDashboard/SideBar'
 
 export const DashboardPage = () => {
   return (
